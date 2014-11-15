9dd817b2c6ab9f5791e296b2230e4a2ac4ec7ed9
Zifeng Xu <xuzifeng89@gmail.com>
2014-11-15 14:56:35 -0500

    modify car and snow land

--- Car/Scripts/objects/car.ts
+++ Car/Scripts/objects/car.ts
@@ -12,7 +12,6 @@ module objects {
             this.stage = stage;
             this.game = game;
             this.image = new createjs.Sprite(managers.Assets.atlas, "car");
-            this.image.y = 430;
             this.width = this.image.getBounds().width;
             this.height = this.image.getBounds().height;
             this.image.regX = this.width / 2;
@@ -24,6 +23,7 @@ module objects {
 
         update() {
             this.image.x = this.stage.mouseX;
+            this.image.y = this.stage.mouseY;
         }
         destroy() {
             this.engineSound.stop();
--- Car/Scripts/objects/land.ts
+++ Car/Scripts/objects/land.ts
@@ -7,7 +7,8 @@ module objects {
         game: createjs.Container;
         width: number;
         height: number;
-        dy: number;
+        dx: number;
+        //car contructor
         constructor(stage: createjs.Stage, game: createjs.Container) {
             this.stage = stage;
             this.game = game;
@@ -16,20 +17,20 @@ module objects {
             this.height = this.image.getBounds().height;
             this.reset();
 
-            this.dy = 5;
+            this.dx = 3;
 
             game.addChild(this.image);
         }
 
         update() {
-            this.image.y += this.dy;
-            if (this.image.y >= 0) {
+            this.image.x -= this.dx;
+            if (this.image.x < -300 ) {
                 this.reset();
             }
         }
 
         reset() {
-            this.image.y = -960;
+            this.image.x = 0;
         }
 
         destroy() {
