57a788c5ef0ec58166dc2e6892704bc107c39313
Zifeng Xu <xuzifeng89@gmail.com>
2014-11-15 12:25:56 -0500

    add assets

---
+++ Car/Scripts/managers/asset.ts
@@ -0,0 +1,35 @@
+﻿module managers {
+    // Image and Sound Manifest;
+    var assetManifest = [
+        //images
+        { id: "again", src: "assets/images/again.png" },
+        { id: "bomb", src: "assets/images/bomb.png" },
+        { id: "car", src: "assets/images/car.png" },
+        { id: "instructions", src: "assets/images/instructions.png" },
+        { id: "land", src: "assets/images/land.jpg" },
+        { id: "play", src: "assets/images/play.png" },
+        { id: "rasberry", src: "assets/images/rasberry.gif" },
+
+        //sounds
+        { id: "engine", src: "assets/sounds/engine.wav" },
+        { id: "bom", src: "assets/sounds/bom.wav" },
+        { id: "rasb", src: "assets/sounds/rasb.wav" }
+    ];
+
+
+    // Asset Manager Class
+    export class Assets {
+        public static manifest;
+        public static data;
+
+        public static loader;
+
+        public static init() {
+            createjs.Sound.initializeDefaultPlugins();
+            this.loader = new createjs.LoadQueue();
+            this.loader.installPlugin(createjs.Sound);
+            this.loader.loadManifest(assetManifest);
+        }
+
+    }
+}  
